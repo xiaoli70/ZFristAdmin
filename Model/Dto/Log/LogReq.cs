f688ff68fe1ae6436e7e3125e5abfac1852af66f
yk l <1006606849@qq.com>
2024-01-25 09:48:32 +0800

    日志

---
+++ Model/Dto/Log/LogReq.cs
@@ -0,0 +1,26 @@
+﻿using Model.Other;
+using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Dto.Log
+{
+    public class LogReq
+    {
+      
+        public string? Title { get; set; }
+        
+        public OperEnum OperType { get; set; }
+        
+       
+        public string? RequestMethod { get; set; }
+       
+        
+        public string? OperUser { get; set; }
+       
+        
+    }
+}
